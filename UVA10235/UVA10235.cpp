3cec53c0f0713d6e6667028498fb0cc0e4d2e498
jackyhobingo <jackyhobingo@gmail.com>
2016-02-24 15:14:20 +0800

    UVA10235
    c++

---
+++ UVA10235/UVA10235.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+bool prime[100000001];
+
+bool isPrime(int);
+int reversing(int);
+void build_prime();
+
+int main()
+{
+	build_prime();
+	int N;
+	while(cin >> N)
+	{
+		if (isPrime(N))
+		{
+			int reverse = reversing(N);
+			if(isPrime(reverse) && reverse != N)
+			{
+				cout << N << " is emirp." << endl;
+			}
+			else
+			{
+				cout << N << " is prime." << endl;
+			}
+
+		}
+		else
+		{
+			cout << N << " is not prime." << endl;
+		}
+	}
+}
+void build_prime()
+{
+	for(int i = 2; i < 10000001; i++)
+	{
+		prime[i] = true;
+	}
+	for(int i = 2; i <= 1000; i++)
+	{
+		int t = i + i;
+		while(t < 10000001)
+		{
+			prime[t] = false;
+			t += i;
+		}
+	}
+}
+bool isPrime(int N)
+{
+	return prime[N];
+}
+
+int reversing(int N)
+{
+	int new_N = 0;
+	while( N > 0)
+	{
+		new_N *= 10;
+		new_N += N % 10;
+		N /= 10;
+	}
+	return new_N;
+}
